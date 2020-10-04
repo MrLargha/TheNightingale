8c021a1108493984577a4ceb858766363c8b3273
MrLargha <cppcodery@gmail.com>
2020-10-04 23:35:17 +0300

    Added chart and dirty data capture

--- app/src/main/java/ru/mrlargha/thenightingale/ui/record/RecordFragment.kt
+++ app/src/main/java/ru/mrlargha/thenightingale/ui/record/RecordFragment.kt
@@ -2,29 +2,36 @@ package ru.mrlargha.thenightingale.ui.record
 
 import android.net.Uri
 import android.os.Bundle
-import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.fragment_record.view.*
+import kotlinx.coroutines.*
 import ru.mrlargha.thenightingale.R
 import ru.mrlargha.thenightingale.databinding.FragmentRecordBinding
+
 
 @AndroidEntryPoint
 class RecordFragment : Fragment() {
 
     private val viewModel: RecordViewModel by viewModels()
     private val navArgs: RecordFragmentArgs by navArgs()
+    private val chartValues: MutableList<Entry> = mutableListOf()
+    private lateinit var binding: FragmentRecordBinding
+    private var collectDataJob: Job? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentRecordBinding.inflate(inflater, container, false)
+        binding = FragmentRecordBinding.inflate(inflater, container, false)
 
         binding.apply {
             recordButton.setOnClickListener {
@@ -33,12 +40,18 @@ class RecordFragment : Fragment() {
         }
 
         viewModel.fileUri = Uri.parse(navArgs.musicFileUri)
-        subscribeUI(binding)
+        subscribeUI()
 
         return binding.root
     }
 
-    private fun subscribeUI(binding: FragmentRecordBinding) {
+    private fun updateChart() {
+        binding.chart.data = LineData(LineDataSet(chartValues.takeLast(100), "Trolololol"))
+//        binding.chart.xAxis.mAxisMinimum = if(chartValues.last().x - 1000 >= 0) chartValues.last().x - 1000 else 0f
+        binding.chart.invalidate()
+    }
+
+    private fun subscribeUI() {
         viewModel.trackInfoLiveData.observe(viewLifecycleOwner) {
             binding.apply {
                 artist.text = it.artist
@@ -62,10 +75,25 @@ class RecordFragment : Fragment() {
             when (recordState) {
                 RecordViewModel.RecordState.RECORDING ->
                     binding.recordButton.setIconResource(R.drawable.stop)
-                        .also { binding.recordButton.text = "ОСТАНОВИТЬ ЗАПИСЬ" }
+                        .also { binding.recordButton.text = "ОСТАНОВИТЬ ЗАПИСЬ" }.also {
+                            collectDataJob = lifecycleScope.launch(Dispatchers.IO) {
+                                var i = 0
+                                while (true){
+                                    i += 10
+                                    launch(Dispatchers.Main) {
+                                        chartValues.add(Entry(i.toFloat(), binding.slider2.value))
+                                        updateChart()
+                                    }
+                                    delay(50)
+                                }
+                            }
+                        }
                 RecordViewModel.RecordState.STOPPED ->
                     binding.recordButton.setIconResource(R.drawable.record)
-                        .also { binding.recordButton.text = "НАЧАТЬ ЗАПИСЬ" }
+                        .also { binding.recordButton.text = "НАЧАТЬ ЗАПИСЬ" }.also {
+                            collectDataJob?.cancel()
+                            chartValues.clear()
+                        }
             }
         }
 
